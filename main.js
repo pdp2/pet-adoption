84f6b0b8976eb65bef637f39fcbd8a527ec1674d
pdp2 <pdp2.paolo@gmail.com>
2024-04-23 09:15:26 +0100

    wip: fetched pet data

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
 async function start() {
-  const weatherResponse = await fetch('https://api.weather.gov/gridpoints/MFL/110,50/forecast')
-  const weatherData = await weatherResponse.json()
-  const ourTemperature = weatherData.properties.periods[0].temperature
-  document.querySelector('#temperature-output').textContent = ourTemperature
+  const weatherResponse = await fetch(
+    "https://api.weather.gov/gridpoints/MFL/110,50/forecast"
+  );
+  const weatherData = await weatherResponse.json();
+  const ourTemperature = weatherData.properties.periods[0].temperature;
+  document.querySelector("#temperature-output").textContent = ourTemperature;
 }
 
-start()
+start();
+
+async function petsArea() {
+  const petsResponse = await fetch(
+    "https://learnwebcode.github.io/bootcamp-pet-data/pets.json"
+  );
+  const petsData = await petsResponse.json();
+  petsData.forEach((pet) => {
+    console.log(pet.name);
+  });
+}
+
+petsArea();
